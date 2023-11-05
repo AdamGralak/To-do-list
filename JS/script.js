8c2f6c1aefc22a471cdb29a8bd33d25a5207dc38
Adam Gralak <gralak.adam2@wp.pl>
2023-11-05 21:54:31 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+{
+    const tasks = [
+        {
+            content: "zadanie 1",
+            done: true,
+        },
+        {
+            content: "zadanie 2",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+              <li>
+                ${task.content};
+              </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+    init();
+}
