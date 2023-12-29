f5a28e0705a42fab8ef885be9dea3721c8a13645
Adam Gralak <gralak.adam2@wp.pl>
2023-12-30 00:15:10 +0100

    added hide done tasks and optional buttons

--- JS/script.js
+++ JS/script.js
@@ -1,54 +1,87 @@
 {
-    let tasks = [
-    ];
+    let tasks = [];
+    let hideTaskDone = false;
 
-    const removeTasks = (index) =>{
-        tasks = tasks.filter((task,i) => i !== index);
+    const toggleHideTaskDone = () => {
+        hideTaskDone = !hideTaskDone;
+        render();
+    };
+
+    const switchHideTaskDone = () => {
+        const hideTaskDoneButton = document.querySelector(".js-switchHideTaskDone");
+        if (hideTaskDoneButton) {
+            hideTaskDoneButton.addEventListener("click", () => {
+                toggleHideTaskDone();
+            });
+        }
+    };
+
+    const removeTasks = (index) => {
+        tasks = tasks.filter((task, i) => i !== index);
         render();
     };
 
     const doneTasks = (index) => {
         tasks = tasks.map((task, i) => {
-          if (i === index) {
-            return { ...task, done: !task.done };
-          }
-          return task;
+            if (i === index) {
+                return { ...task, done: !task.done };
+            }
+            return task;
         });
         render();
-      };
+    };
 
-    const bindEvents = () =>{
+    const renderOptionsButtons = () => {
+        if (tasks.length !== 0) {
+            let optionsButtons = "";
+            optionsButtons += `
+            <button class="section__optionButton"> Ukończ wszystkie</button>
+            <button class="section__optionButton js-switchHideTaskDone"> ${hideTaskDone ? "Wyświetl ukończone" : "Ukryj ukończone"}</button>
+        `;
+            document.querySelector(".js-optionTasksButton").innerHTML = optionsButtons;
+        }
+        else {
+            document.querySelector(".js-optionTasksButton").innerHTML = "";
+        }
+        switchHideTaskDone();
+    };
+
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
         const doneButtons = document.querySelectorAll(".js-done");
 
-        removeButtons.forEach((removeButtons, index) =>{
-            removeButtons.addEventListener("click", () =>{
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
                 removeTasks(index);
             });
         });
 
-        doneButtons.forEach((doneButtons, index) =>{
-            doneButtons.addEventListener("click", () =>{
+        doneButtons.forEach((doneButtons, index) => {
+            doneButtons.addEventListener("click", () => {
                 doneTasks(index);
             });
         });
-    }
-    
-    const render = () => {
+    };
+
+    const renderTasks = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
-              <li class= "section__task">
+              <li class= "section__task ${task.done && hideTaskDone ? "section__task-hidden" : ""}">
               <button class="js-done task__button"> ${task.done ? "✓" : ""}</button>
               <span class="list__item ${task.done ? "list__item--done" : ""}">
               ${task.content}</span>
                 <button class="js-remove task__delete">🗑️</button>
               </li>
             `;
-
-        }
+        };
         document.querySelector(".js-tasks").innerHTML = htmlString;
         bindEvents();
+    };
+
+    const render = () => {
+        renderTasks();
+        renderOptionsButtons();
     };
 
     const addNewTask = (inputContent) => {
@@ -57,16 +90,16 @@
         if (newTaskContent === "") {
             return;
         }
-        
+
         tasks = [
             ...tasks,
-            {content: newTaskContent},
+            { content: newTaskContent },
         ];
 
-        inputContent.value= "";
+        inputContent.value = "";
         render();
     };
-    
+
     const onFormSubmit = (event) => {
         event.preventDefault();
         const inputContent = document.querySelector(".js-taskContent");
